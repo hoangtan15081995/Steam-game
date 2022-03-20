0fd4b749427bf1229afc56180f11464d0d973bf6
hoangtan15081995 <98004746+hoangtan15081995@users.noreply.github.com>
2022-03-20 17:52:08 +0700

    update 18h 20/3

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const getGenresList = async () => {
-  try {
-    const response = await fetch('https://cs-steam-api.herokuapp.com/genres');
-    const genres = await response.json();
-    return genres;
-  } catch (err) {
-    console.log(err)
-  }
-}
-getGenresList();
-
-// const renderGenresList = async () => {
-//   try {
-//     const genres = await getGenresList();
-//     const ulGenresList = document.querySelector(".categoryGroup");
-//     ulGenresList.innerHTML = "";
-//   genres.data.forEach((gen) => {
-//     const li = document.createElement("li");
-//     li.innerHTML = `${gen.name.toUpperCase()}`;
-//     ulGenresList.appendChild(li);
-//   })
-//   } catch (err) {
-//     console.log(err);
-//   }
-// }
-// renderGenresList();
-
 
 const getAllGames = async () => {
   try {
@@ -35,27 +8,29 @@ const getAllGames = async () => {
     console.log(err)
   }
 }
-getGenresList();
 
 const renderAllGames = async () => {
   try {
     const allGames = await getAllGames();
     const showGames = document.querySelector(".showGames");
-    
-  allGames.data.forEach((game) => {
-    const div = document.createElement("div");
-    div.innerHTML = `<div class="background">
-            <div class="game_wrapper">
-              <div class="cover">
-                <img src="${game.header_image}">
-                <div class="game_info">
-                  <p>${game.name}</p>
-                  <p>${game.price} $</p>
-                </div>
-              </div>
-             </div>
-          </div>`;
-    showGames.appendChild(div);
+    allGames.data.forEach((game, index) => {
+    const app = allGames.data[index].appid;
+    const appid = document.createElement("div");
+    appid.innerHTML = `<div class="background">
+                       <div class="game_wrapper">
+                         <div class="cover">
+                           <img src="${game.header_image}">
+                           <div class="game_info">
+                             <p>${game.name}</p>
+                             <p>${game.price} $</p>
+                           </div>
+                         </div>
+                       </div>
+                     </div>`;
+    showGames.appendChild(appid);
+    appid.addEventListener("click", () => {
+      renderDetailGame(app);
+      })
   })
   } catch (err) {
     console.log(err);
@@ -64,8 +39,43 @@ const renderAllGames = async () => {
 renderAllGames();
 
 
-const button = document.querySelector('#btn');
+const renderDetailGame = async (app) => {
+  try {
+    const number = parseInt(app);
+    const url = `https://cs-steam-api.herokuapp.com/single-game/${number}`;
+    const resp = await fetch(url);
+    const detailGames = await resp.json();
+    const showGames = document.querySelector(".showGames");
+    showGames.innerHTML = "";
+    // const detail = document.createElement("div");
+    showGames.innerHTML =`<div class="showDetailGame" background-image: url("${ detailGames.data.background})">
+            <div class="detail">
+              <div class="name">
+                <h2>${detailGames.data.name}</h2>
+                <p>Price:${ detailGames.data.price }</p>
+              </div>
+              <div class="picture">
+                <div class="img">
+                  <img src="${detailGames.data.header_image}" alt="picture">
+                </div>
+                <div class="text">
+                  <p>${detailGames.data.description}</p>
+                  
+                </div>
+              </div>
+              <div class="seeMore">
+                <a href="">About more</a>
+                <a href="">Get</a>
+              </div>
+            </div>
+        </div>
+      </div> `
+  } catch (err) {
+    console.log(err);
+  }
+}
 
+const button = document.querySelector('#btn');
 button.addEventListener('click', () => {
   renderListGamesToInput();
 });
@@ -75,61 +85,76 @@ const renderListGamesToInput = async () => {
     const input = document.querySelector("#searchForm");
     const inputValue = input.value;
     const url = `https://cs-steam-api.herokuapp.com/games?q=${inputValue}&limit=25`
-      const res = await fetch(url);
-      const listGames = await res.json();
-      const showGames = document.querySelector(".showGames");
-      showGames.innerHTML = "";
-      listGames.data.forEach((game) => {
-        const div = document.createElement("div");
-        div.innerHTML = `<div class="background">
-              <div class="game_wrapper">
-                <div class="cover">
-                  <img src="${game.header_image}">
-                  <div class="game_info">
-                    <p>${game.name}</p>
-                    <p>${game.price} $</p>
-                  </div>
-                </div>
-               </div>
-            </div>`;
-        showGames.appendChild(div);
+    const res = await fetch(url);
+    const listGames = await res.json();
+    const showGames = document.querySelector(".showGames");
+    showGames.innerHTML = "";
+    listGames.data.forEach((game, index) => {
+    const app = listGames.data[index].appid;
+    const div = document.createElement("div");
+    div.innerHTML = `<div class="background">
+                       <div class="game_wrapper">
+                         <div class="cover">
+                           <img src="${game.header_image}">
+                           <div class="game_info">
+                             <p>${game.name}</p>
+                             <p>${game.price} $</p>
+                           </div>
+                         </div>
+                       </div>
+                     </div>`;
+      showGames.appendChild(div);
+      div.addEventListener("click", () => {
+        renderDetailGame(app);
+      })
       })
     } catch (err) {
       console.log(err);
     }
 }
-// const ulGenresList = document.querySelector(".categoryGroup");
-// const liGenres = ulGenresList.children[1];
+
+
 const liGenres = document.querySelectorAll(".genres");
 console.log(liGenres);
 
-liGenres[0].addEventListener("click", () => {
-  console.log("hehe");
+liGenres.forEach((e, index) => {
+  e.addEventListener("click", () => {
+    renderListGamesToGenres(index);
+  })
 })
-liGenres[1].addEventListener("click", () => {
-  console.log("ahaa");
-})
-liGenres[2].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[3].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[4].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[5].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[6].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[7].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[8].addEventListener("click", () => {
-  console.log("hehe");
-})
-liGenres[9].addEventListener("click", () => {
-  console.log("hehe");
-})
+
+const renderListGamesToGenres = async (index) => {
+  try {
+    const genres = liGenres[index].innerHTML.toLowerCase();
+    // console.log(genres)
+    const url = `https://cs-steam-api.herokuapp.com/games?genres=${genres}&limit=25`;
+    const res = await fetch(url);
+    const listGames = await res.json();
+    // console.log(listGames);
+    const showGames = document.querySelector(".showGames");
+    showGames.innerHTML = "";
+    listGames.data.forEach((game) => {
+      const app = listGames.data[index].appid;
+      const div = document.createElement("div");
+    div.innerHTML = `<div class="background">
+                        <div class="game_wrapper">
+                           <div class="cover">
+                             <img src="${game.header_image}">
+                             <div class="game_info">
+                                <p>${game.name}</p>
+                                <p>${game.price} $</p>
+                             </div>
+                          </div>
+                       </div>
+                     </div>`;
+      showGames.appendChild(div);
+      div.addEventListener("click", () => {
+        renderDetailGame(app);
+      })
+      })
+    } catch (err) {
+      console.log(err);
+    }
+}
+
+
